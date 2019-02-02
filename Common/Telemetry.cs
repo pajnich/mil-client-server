628643eac4faa00a19d52c90f3e72e5db3297fad
Matic P <matic.pajnic@gmail.com>
2019-02-02 08:20:03 +0100

    First commit. Added gitignore.

---
+++ Common/Telemetry.cs
@@ -0,0 +1,11 @@
+﻿namespace Common
+{
+    class Telemetry
+    {
+        public double Longitude { get; set; }
+
+        public double Latitude { get; set; }
+
+        public float Altitude { get; set; }
+    }
+}
---
+++ Common/Unit.cs
@@ -0,0 +1,61 @@
+﻿using System.Configuration;
+
+namespace Common
+{
+    class Unit
+    {
+        public enum HostilityEnum : sbyte
+        {
+            /// <summary>
+            /// Unspecified Hostility status
+            /// </summary>
+            UNSPECIFIED = -1,
+            /// <summary>
+            /// Unknown Hostility status
+            /// </summary>
+            UNKNOWN = 0,
+            /// <summary>
+            /// Friendly status
+            /// </summary>
+            FRIEND,
+            /// <summary>
+            /// Neutral status
+            /// </summary>
+            NEUTRAL,
+            /// <summary>
+            /// Hostile status
+            /// </summary>
+            HOSTILE
+        }
+
+        public ulong Id { get; set; }
+
+        [StringValidator(MaxLength = 21)]
+        public string Designation { get; set; }
+        
+        public Telemetry Telemetry { get; set; }
+
+        [StringValidator(MaxLength = 20)]
+        public string StaffComment { get; set; }
+
+        [StringValidator(MaxLength = 20)]
+        public string AdditionalInfo { get; set; }
+
+        public HostilityEnum Hostility { get; set; }
+
+        [IntegerValidator(MinValue = 0, MaxValue = 100)]
+        public byte StatusAmmo { get; set; }
+
+        [IntegerValidator(MinValue = 0, MaxValue = 100)]
+        public byte StatusPersonel { get; set; }
+
+        [IntegerValidator(MinValue = 0, MaxValue = 100)]
+        public byte StatusWeapons { get; set; }
+
+        [IntegerValidator(MinValue = 0, MaxValue = 100)]
+        public byte StatusPOL { get; set; }
+
+        [StringValidator(MaxLength = 20)]
+        public string EquipmentType { get; set; }
+    }
+}
